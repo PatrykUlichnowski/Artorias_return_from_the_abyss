821025f43f49cd9ed35959949536c42fa1f91691
pykir74 <pykir74@gmail.com>
2024-06-21 16:36:12 +0200

    New graphics and new projectile system

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireballs;
+    private Animator anim;
+    private PlayerMovement playerMovement;
+    private float cooldownTimer = 9999;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && cooldownTimer > attackCooldown && playerMovement.canAttack())
+        {
+            Attack();
+        }
+        cooldownTimer += Time.deltaTime;
+    }
+
+    private void Attack()
+    {
+        anim.SetTrigger("Attack");
+        cooldownTimer = 0;
+        /*
+         * Object Pooling = you can create and then deactivate many objects
+         * Instantiate and destroy = creating new object everytime action is performed/it's easier to implement but have bad performence
+         */
+        //pooling fireballs
+
+        fireballs[FindFireball()].transform.position = firePoint.position;
+        fireballs[FindFireball()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+
+    private int FindFireball()
+    {
+        for (int i = 0; i < fireballs.Length; i++)
+        {
+            //if fireball[i] is NOT used you can "send it"
+            if (!fireballs[i].activeInHierarchy)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,30 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D body;
     //SerializeField is used so that you can operate on the variable from Unity 
     [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+    private Rigidbody2D body;
     private Animator anim;
-    private bool grounded = true;
+    private BoxCollider2D boxCollider;
+    private float wallJumpCooldown;
+    private float horizontalInput;
+
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        //left and right movement
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        horizontalInput = Input.GetAxis("Horizontal");
 
         //jumping
-        if (Input.GetKey(KeyCode.Space) && grounded)
-        {
-            Jump();
-        }
+        
 
         // fliping character sprite left and right as player moves
         if (horizontalInput > 0.01f)
@@ -39,7 +41,34 @@ public class NewBehaviourScript : MonoBehaviour
 
         //setting up animations
         anim.SetBool("Run", horizontalInput != 0);
-        anim.SetBool("Grounded", grounded);
+        anim.SetBool("Grounded", isGrounded());
+
+        //so that the player doesnt spam wall jump
+        if(wallJumpCooldown > 0.2f)
+        { 
+            //left and right movement
+            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+            if (onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
+            }
+            else
+            {
+                //so that character dont fly around
+                body.gravityScale = 3;
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            wallJumpCooldown += Time.deltaTime;
+        }
     }
     // Start is called before the first frame update
     void Start()
@@ -48,15 +77,46 @@ public class NewBehaviourScript : MonoBehaviour
     } 
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x, speed);
-        anim.SetTrigger("Jump");
-        grounded = false;
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
+        if (isGrounded())
         {
-            grounded = true;
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            anim.SetTrigger("Jump");
         }
+        else if (onWall() && !isGrounded())
+        {
+            if(horizontalInput == 0)
+            {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                //applying force away from the wall and up
+                /*
+                  Sign returns 1 if we are facing right and -1 if left, we must flip those values to perform wall jump;
+                  then we apply the force which pushes the player away from the wall and up
+                */
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+            }
+            //setting zero so that player needs to wait a bit
+            wallJumpCooldown = 0; 
+        }
+    }
+    private bool isGrounded()
+    {
+        //box that checks if character is standing on collision
+        //                                          center, size, rotation, where to check with the box, where to position, layer mask
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
+    }
+
+    public bool canAttack()
+    {
+        return horizontalInput == 0 && isGrounded() && !onWall();
     }
 }
